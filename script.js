56be2b930cd6136baa96a5a4d1afd3a7a801d598
amirhon23 <amirhonahmadov23@gmail.com>
2025-02-07 17:59:53 +0300

    upload

---
+++ script.js
@@ -0,0 +1,111 @@
+// let a = +prompt("введите число 1")
+// let operator = prompt("* / - + ** %")
+// let b = +prompt("введите число 2")
+
+
+// if (operator === "*") {
+//     console.log(a * b);
+// } else if (operator === "/") {
+//     console.log(a / b);
+// } else if (operator === "+") {
+//     console.log(a + b);
+// } else if (operator === "-") {
+//     console.log(a - b);
+// } else {
+//     console.log("Ошибка, в операторе!");
+// }
+// let a = 6
+// if (a % 2 === 0) {
+//     console.log("Четное");
+// } else {
+//     console.log(a + " не четное число");
+// }
+
+
+// let currentYear = 2020
+// console.log(currentYear % 4);
+
+
+// if (currentYear % 4 === 0) {
+//     console.log(currentYear + " вискостный");
+// } else {
+//     console.log(currentYear + " невискостный");
+// }
+// switch (проверяемая перемнная) //if {
+//     case "чему равняется": // === *
+//         // действие при выполнения условий
+//         break; //остановка
+
+//     default: //else
+//         // действие при невыполнения условий
+//         break; //остановка
+// }
+// switch (operator) {
+//     case "*":
+//         console.log(a * b);
+//         break;
+//     case "/":
+//             console.log(a / b);
+//             break;
+//     case "+":
+//         console.log(a + b);
+//         break;
+//     case "-":
+//         console.log(a - b);
+//         break;
+//     default:
+//         console.log("Ошибка, в операторе!");
+//         break;
+// }
+
+
+//1
+let month = +prompt("Введите месяц с 1 до 12")
+//
+if (month === 1) {
+    console.log("Январь");
+} else if (month === 2) {
+    console.log("Февраль");
+} else if (month === 3) {
+    console.log("Март");
+} else if (month === 4) {
+    console.log("Апрель");
+} else if (month === 5) {
+    console.log("Май");
+} else if (month === 6) {
+    console.log("Июнь");
+} else if (month === 7) {
+    console.log("Июль");
+} else if (month === 8) {
+    console.log("Август");
+} else if (month === 9) {
+    console.log("Сентябрь");
+} else if (month === 10) {
+    console.log("Октябрь");
+} else if (month === 11) {
+    console.log("Ноябрь");
+} else if (month === 12) {
+    console.log("Декабрь");
+} else {
+    console.log("Вы вели другое число, попробуйте попытку.");
+}
+//2
+let week = +prompt("Введите день недели с 1 до 7")
+//
+if (week === 1) {
+    console.log("Понедельник");
+} else if (week === 2) {
+    console.log("Вторник");
+} else if (week === 3) {
+    console.log("Среда");
+} else if (week === 4) {
+    console.log("Четверг");
+} else if (week === 5) {
+    console.log("Пятница");
+} else if (week === 6) {
+    console.log("Суббота");
+} else if (week === 7) {
+    console.log("Воскресенье");
+}
+
+
